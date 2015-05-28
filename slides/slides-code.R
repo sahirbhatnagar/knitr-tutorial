ebcf9a4665a9be912549c3124656e94b5277a091
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2015-05-28 12:41:19 -0400

    cleaned folders

--- slides/slides-code.R
+++ slides/slides-code.R
@@ -1,42 +1,39 @@
-#####################################
-# R Source code for knitr-tutorial slide
-# Created: May 5, 2015
-# Updated: May 24, 2015
-# NOTES:
-#####################################
+## ---- required-packages ----
 
-
-## ---- functions ----
 getPckg <- function(pckg) install.packages(pckg, repos = "http://cran.r-project.org")
-library(broom)
-library(dplyr)
-library(xtable)
-#library(stargazer)
-library(texreg)
-library(lme4)
-library(MASS)
-library(geepack)
-data(epil)
-source("~/git_repositories/knitr-tutorial/slides/texreg-geepack.R")
 
-## ---- example ----
-lmfit <- lm(mpg ~ wt, mtcars)
-stargazer::stargazer(lmfit, digits = 2)
-jj <- extract(lmfit)
-jj@ci.low
+pckg = try(require(knitr))
+if(!pckg) {
+    cat("Installing 'knitr' from CRAN\n")
+    getPckg("knitr")
+    require(knitr)
+}
 
+pckg = try(require(xtable))
+if(!pckg) {
+    cat("Installing 'xtable' from CRAN\n")
+    getPckg("xtable")
+    require(xtable)
+}
 
-## ---- example-2 ----
-fit.glm <- glm(y ~ trt*period + age, 
-                  data = epil, family = poisson)
-fit.glmm <- glmer(y ~ trt*period + age + (1|subject), 
-             data = epil, family = poisson)
-fit.gee <- geeglm(y ~ trt*period + age, id = subject, 
-                  data = epil, family = poisson, corstr = "unstructured")
-texreg(list(fit.glm,fit.glmm,fit.gee), custom.model.names = c("GLM","GLMM","GEE"), 
-       custom.coef.names = c("Intercept","progabide","time","age","progabide:time"),
-       fontsize = "scriptsize", caption = "Comparing model estimates", single.row = T,
-       bold = 0.05, ci.force = TRUE)
+pckg = try(require(data.table))
+if(!pckg) {
+    cat("Installing 'data.table' from CRAN\n")
+    getPckg("data.table")
+    require(data.table)
+}
 
+pckg = try(require(dplyr))
+if(!pckg) {
+    cat("Installing 'dplyr' from CRAN\n")
+    getPckg("dplyr")
+    require(dplyr)
+}
 
 
+pckg = try(require(ggplot2))
+if(!pckg) {
+    cat("Installing 'ggplot2' from CRAN\n")
+    getPckg("ggplot2")
+    require(ggplot2)
+}
